b2d36eaa6bc112c133d1b9003a15a7e29efde928
locbgiang <locbgiang@gmail.com>
2025-10-15 12:51:29 -0700

    finished HelperConfig.s.sol

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -29,8 +29,12 @@ contract HelperConfig is Script {
     uint256 constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 constant ZKSYNC_SEPOLIA_CHAIN_ID = 300;
     uint256 constant LOCAL_CHAIN_ID = 31337;
+
     // Update the BURNER_WALLET to your burner wallet!
-    //address constant BURNER_WALLET = 
+    address constant BURNER_WALLET = 0x352391E0B031D7a6E82C85fb8096fb3FCC347253;
+
+    uint256 constant ARBITRUM_MAINNET_CHAIN_ID = 42_161;
+    uint256 constant ZKSYNC_MAINNET_CHAIN_ID = 324;
 
     address constant ANVIL_DEFAULT_ACCOUNT = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
 
@@ -43,9 +47,16 @@ contract HelperConfig is Script {
 
     constructor() {
         // get the sepolia network config
+        networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getEthSepoliaConfig();
+
         // get the mainnet config
+        networkConfigs[ETH_MAINNET_CHAIN_ID] = getEthMainnetConfig();
+
         // get the zksync config
+        networkConfigs[ZKSYNC_MAINNET_CHAIN_ID] = getZksyncConfig();
+
         // get the arbitrum config
+        networkConfigs[ARBITRUM_MAINNET_CHAIN_ID] = getArbMainnetConfig();
     }
 
     function getConfig() public returns (NetworkConfig memory) {
@@ -69,8 +80,42 @@ contract HelperConfig is Script {
     function getEthMainnetConfig() public pure returns (NetworkConfig memory) {
         // this is v7
         return NetworkConfig({
-            entryPoint: 
-        })
+            entryPoint: 0x0000000071727De22E5E9d8BAf0edAc6f37da032,
+            usdc: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
+            account: BURNER_WALLET
+        });
+    }
+
+    function getEthSepoliaConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({
+            entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789,
+            usdc: 0x53844F9577C2334e541Aec7Df7174ECe5dF1fCf0, // Update with your own mock token
+            account: BURNER_WALLET
+        });
+    }
+
+    function getArbMainnetConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({
+            entryPoint: 0x0000000071727De22E5E9d8BAf0edAc6f37da032,
+            usdc: 0xaf88d065e77c8cC2239327C5EDb3A432268e5831,
+            account: BURNER_WALLET
+        });
+    }
+
+    function getZkSyncSepoliaConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({
+            entryPoint: address(0), // there is no entryPoint in zksync
+            usdc: 0x5A7d6b2F92C77FAD6CCaBd7EE0624E64907Eaf3E, // not the real USDC on zksync sepolia
+            account: BURNER_WALLET
+        });
+    }
+
+    function getZksyncConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({
+            entryPoint: address(0),
+            usdc: 0x1d17CBcF0D6D143135aE902365D2E5e2A16538D4,
+            account: BURNER_WALLET
+        });
     }
 
     function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory) {
