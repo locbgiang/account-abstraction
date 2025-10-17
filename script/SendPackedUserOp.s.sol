904f17326316f0bb4dd3bc322bfddd01e0aa51bb
Loc Giang <locbgiang@gmail.com>
2025-10-16 22:39:58 -0700

    finish sendPackedUserOp

--- script/SendPackedUserOp.s.sol
+++ script/SendPackedUserOp.s.sol
@@ -6,16 +6,28 @@ import {HelperConfig} from "./HelperConfig.s.sol";
 import {DevOpsTools} from "@foundry-devops/src/DevOpsTools.sol";
 
 contract SendPackedUserOp is Script {
+
+    /**
+     * designed to create and send a 'packed user operation' on the Ethereum blockchain.
+     */
     function run () public {
         // setup
+        // A HelperConfig instance is created to retrieve configuration details
         HelperConfig helperConfig = new HelperConfig();
         address dest = helperConfig.getConfig().usdc; // arbitrum mainnnet USDC address
+        
+        // the value is set to 0, meaning no native Ether will be sent with the operation
         uint256 value = 0;
+
+        // the address of the most recent deployment of the MinimalAccount contract
+        // retrieved using DevOpsTools
         address minimalAccountAddress = DevOpsTools.get_most_recent_deployment(
             "MinimalAccount",
             block.chainid
         );
 
+        // the approve function of the IERC20 interface is encoded with the following parameters
+        // RANDOM_APPROVER: The address
         bytes memory functionData = abi.encodeWithSelector(IERC20.approve.selector, RANDOM_APPROVER, 1e18);
         bytes memory executionCallData = abi.encodeWithSelector(
             MinimalAccount.execute.selector,
@@ -35,5 +47,58 @@ contract SendPackedUserOp is Script {
         vm.startBroadcast();
         IEntryPoint(helperConfig.getConfig().entryPoint).handleOps(ops, payable(helperConfig.getConfig().account));
         vm.stopBroadcast();
+    }
+
+    function generateSignedUserOperation(
+        bytes memory callData,
+        HelperConfig.NetworkConfig memory config,
+        address minimalAccount
+    ) public view returns (PackedUserOperation memory) {
+        // 1. Generate the unsigned data
+        uint256 nonce = IEntryPoint(config.entryPoint).getNonce(minimalAccount, 0);
+        PackedUserOperation memory userOp = _generateUnsignedUserOperation(callData, minimalAccount, nonce);
+
+        // 2. Get the userOp hash
+        bytes32 userOpHash = IEntryPoint(config.entryPoint).getUserOpHash(userOp);
+        bytes32 digest = userOpHash.toEthSignedMessageHash();
+
+        // 3. Sign it
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+        uint256 ANVIL_DEFAULT_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+        if (block.chainid == 31337) {
+            (v, r, s) = vm.sign(ANVIL_DEFAULT_KEY, digest);
+        } else {
+            (v, r, s) = vm.sign(config.account, digest);
+        }
+        userOp.signature = abi.encodePacked(r, s, v) // note the order
+        return userOp;
+    }
+
+    function _generateUnsignedUserOperation(
+        bytes memory callData, 
+        address sender, 
+        uint256 nonce
+    ) 
+        internal
+        pure
+        returns (PackedUserOperation memory) 
+    {
+        uint128 verificationGasLimit = 16777216;
+        uint128 callGasLimit = verificationGasLimit;
+        uint128 maxPriorityFeePerGas = 256;
+        uint128 maxFeePerGas = maxPriorityFeePerGas;
+        return PackedUserOperation({
+            sender: sender,
+            nonce: nonce,
+            initCode: hex"",
+            callData: callData,
+            accountGasLimits: bytes32(uint256(verificationGasLimit) << 128 | callGasLimit),
+            preVerificationGas: verificationGasLimit,
+            gasFees: bytes32(uint256(maxPriorityFeePerGas) << 128 | maxFeePerGas),
+            paymasterAndData: hex"",
+            signature: hex""
+        });
     }
 }
