03d0191b153636d1cd0fd25592efa90645d9b6ae
Loc Giang <locbgiang@gmail.com>
2025-10-22 13:01:35 -0700

    test init

--- test/ethereum/MinimalAccountTest.t.sol
+++ test/ethereum/MinimalAccountTest.t.sol
@@ -3,6 +3,38 @@ pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
 
+import {MinimalAccount} from "../../src/ethereum/MinimalAccount.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {SendPackedUserOp} from "../../script/SendPackedUserOp.s.sol";
+import {DeployMinimal} from "../../script/DeployMinimal.s.sol";
+
 contract MinimalAccountTest is Test {
-    
+    MinimalAccount minimalAccount;
+    HelperConfig helperConfig;
+    ERC20Mock usdc;
+    SendPackedUserOp sendPackedUserOp;
+
+    function setUp() public {
+        DeployMinimal deployMinimal = new DeployMinimal();
+        (helperConfig, minimalAccount) = deployMinimal.deployMinimalAccount();
+        usdc = new ERC20Mock();
+        sendPackedUserOp = new SendPackedUserOp();
+    }
+
+    function testOwnerCanExecuteCommands() public {
+        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(
+            ERC20Mock.mint.selector, 
+            address(minimalAccount),
+            AMOUNT
+        );
+
+        vm.prank(minimalAccount.owner());
+        minimalAccount.execute(dest, value, functionData);
+
+        assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
+    }
 }
